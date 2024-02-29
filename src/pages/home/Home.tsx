be1257e9af9052a93ccb777dcb064e0f195c4339
unknown <leonardo_3951@UIRAPURU.BR>
2024-02-28 22:17:50 -0300

    Inicialização do Crud

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,7 +6,7 @@ function Home() {
     return (
     
         <div className="bg-white">
-          <div className="pt-10 scroll-px-10 lg:px-8">
+          <div className="p-10 scroll-px-10 lg:px-8">
             <div className="relative isolate overflow-hidden bg-white px-6 pt-16 shadow-2xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
               <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
                 <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
